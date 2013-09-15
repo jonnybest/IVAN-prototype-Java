e4165bd3a61a16fe6b68191f61c4701b4711d609
jonnybest <jonathan.best@student.kit.edu>
2013-09-15 16:52:50 +0200

    does not display lines

--- src/SwingWindow.java
+++ src/SwingWindow.java
@@ -1,22 +1,17 @@
 import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.Font;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 import javax.swing.BoxLayout;
-import javax.swing.CodeEditorPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.JTextPane;
 import javax.swing.LineNumbersTextPane;
-import javax.swing.event.DocumentListener;
-import javax.swing.event.UndoableEditListener;
+import javax.swing.LineNumbersTextPane.LineNumbersSidePane;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
-import javax.swing.text.DefaultStyledDocument;
-import javax.swing.text.Element;
-import javax.swing.text.Position;
-import javax.swing.text.Segment;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
@@ -37,8 +32,6 @@ import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.util.CoreMap;
-import java.awt.FlowLayout;
-
 
 public class SwingWindow {
 
@@ -46,8 +39,9 @@ public class SwingWindow {
 	private JXFrame frame;
 	private LineNumbersTextPane txtEditor;
 	StyleContext sc = new StyleContext();
-    //final DefaultStyledDocument doc = new DefaultStyledDocument(sc);
+	// final DefaultStyledDocument doc = new DefaultStyledDocument(sc);
 	private Style SetupStyle;
+	protected AttributeSet DefaultStyle;
 
 	/**
 	 * Launch the application.
@@ -77,40 +71,47 @@ public class SwingWindow {
 	 */
 	private void initialize() {
 		frame = new JXFrame();
-		frame.setBounds(100, 100, 536, 300);
+		frame.setBounds(100, 100, 612, 511);
 		frame.setDefaultCloseOperation(JXFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
-		
-		JPanel panel = new JXPanel();
-		frame.getContentPane().add(panel);		
-		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-		
-		txtEditor = new CodeEditorPane();
-		txtEditor.setDisplayLineNumbers(true);
-		//txtEditor.setDocument(doc);
+		frame.getContentPane().setLayout(
+				new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 
-		txtEditor.setText("In publishing and graphic design, lorem ipsum is a \nplaceholder text (filler text) commonly used to demonstrate \nthe graphic elements of a document or visual presentation, \nsuch as font, typography, and layout, by removing the \ndistraction of meaningful content.");
-		
+		JPanel inputareaPanel = new JXPanel();
+		frame.getContentPane().add(inputareaPanel);
+		inputareaPanel
+				.setLayout(new BoxLayout(inputareaPanel, BoxLayout.X_AXIS));
+
+		txtEditor = new LineNumbersTextPane();
+		txtEditor.setDisplayLineNumbers(true);
+		// txtEditor.setDocument(doc);
+
+		txtEditor.setText("In publishing and graphic design, lorem ipsum is a \n"
+				+ "placeholder text (filler text) commonly used to demonstrate \n"
+				+ "the graphic elements of a document or visual presentation, \n"
+				+ "such as font, typography, and layout, by removing the \n"
+				+ "distraction of meaningful content.");
+
 		addStylesToDocument((StyledDocument) txtEditor.getDocument());
+
+		inputareaPanel.add(txtEditor.getContainerWithLines());
 		
-		panel.add(txtEditor.getContainerWithLines());
-		
-		JXTaskPaneContainer taskContainer = new JXTaskPaneContainer();		
+
+		JXTaskPaneContainer taskContainer = new JXTaskPaneContainer();
 		JXTaskPane taskPane = new JXTaskPane();
 		JXLabel taskLabel = new JXLabel("Nothing to do right now.");
 		taskPane.add(taskLabel);
 		taskContainer.add(taskPane);
-		panel.add(taskContainer);
-		
-		JTextPane txtpnHi = new JTextPane();
-		txtpnHi.setEditable(false);
-		txtpnHi.setText("...");
-		frame.getContentPane().add(txtpnHi);
-		
+		inputareaPanel.add(taskContainer);
+
+		JTextPane emitterTextField = new JTextPane();
+		emitterTextField.setEditable(false);
+		emitterTextField.setText("...");
+		frame.getContentPane().add(emitterTextField);
+
 		txtEditor.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyTyped(KeyEvent arg0) {
-				if (arg0.getKeyChar() == '.' || arg0.getKeyChar() == '\n') {				
+				if (arg0.getKeyChar() == '.' || arg0.getKeyChar() == '\n') {
 					JXEditorPane editor = (JXEditorPane) arg0.getSource();
 					try {
 						startStopWatch();
@@ -119,116 +120,134 @@ public class SwingWindow {
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					}					
-				}
-			}
-			
-			private void stopAndPrintStopWatch() {
-				// TODO Auto-generated method stub
-				org.joda.time.DateTime now = org.joda.time.DateTime.now();
-				long diff = now.getMillis() - stopwatch.getMillis();
-				System.err.println("Stopwatch: " + (diff) + " ms.");
-			}
-
-			private void startStopWatch() {
-				stopwatch = org.joda.time.DateTime.now();
-			}
-
-			/**
-			 * @param text
-			 * @throws Exception 
-			 */
-			private void processText(String text) throws Exception {				
-				/* Recognise modal sentences 
-				 */
-				StaticDynamicClassifier myclassifier = StaticDynamicClassifier.getInstance();
-				String lines = text;
-				/*String[] modalVerbs = {"can", "could", "may", "might", "must", "shall", "should", "will", "would", "have to", "has to", "had to", "need"};
-				for (String string : modalVerbs) {
-					if (lines.contains(string)) {
-						System.out.println("Found bad word: " + string + ".");						
 					}
-				}*/
-				/* tag with pos tags */
-
-			    StanfordCoreNLP pipeline = null;
-				try {					
-					pipeline = myclassifier.getPipeline();
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
 				}
-			    Annotation mydoc = new Annotation(lines);
-			    pipeline.annotate(mydoc);
-			    java.util.List<CoreMap> sentences = mydoc.get(SentencesAnnotation.class);
-			    
-			    
-			    for(CoreMap sentence: sentences)
-			    {
-			    	// traversing the words in the current sentence
-			    	// a CoreLabel is a CoreMap with additional token-specific labels
-			    	for (CoreLabel item : sentence.get(TokensAnnotation.class)) {
-			    		// this is the text of the token
-			            //String word = item.get(TextAnnotation.class);
-			            // this is the POS tag of the token
-			            String pos = item.get(PartOfSpeechAnnotation.class);
-			            if (pos.equals("VBG")) {
-							System.out.println(item.getString(TextAnnotation.class) + " is a gerund.");
-							markText(item.beginPosition(), item.endPosition());
-						}
-			            // this is the NER label of the token
-			            //String ne = item.get(NamedEntityTagAnnotation.class);
-			            
-			            //System.out.println(word);
-					}
-			    }
-			    
-			    
-			}
-
-			private void markText(int beginPosition, int endPosition) {		
-				int length = endPosition - beginPosition;
-				StyledDocument doc = (StyledDocument) txtEditor.getDocument();
-				try {
-					//System.out.println(txtEditor.getText(beginPosition, length));
-					System.err.println(doc.getText(beginPosition, length));
-				} catch (BadLocationException e) {
-					System.err.println("Bad location: " + beginPosition + " " +  endPosition);
-				}
-				
-				doc.setCharacterAttributes(beginPosition, length, doc.getStyle("action"), true);								
-				nop();
-			}
-
-			private void nop() {
-				// TODO Auto-generated method stub
-				
 			}
 		});
+
+		// LineWrapEditorKit mykit = (LineWrapEditorKit)
+		// txtEditor.getEditorKit();
+		// mykit.setWrap(true);
+		txtEditor.updateLineNumberView();
+		txtEditor.requestFocusInWindow();
 	}
-	
+
 	protected void addStylesToDocument(StyledDocument doc) {
-        //Initialize some styles.
-        Style def = StyleContext.getDefaultStyleContext().
-                        getStyle(StyleContext.DEFAULT_STYLE);
+		// Initialize some styles.
+		Style def = StyleContext.getDefaultStyleContext().getStyle(
+				StyleContext.DEFAULT_STYLE);
 
-        SetupStyle = doc.addStyle("setup", def);
-        StyleConstants.setBackground(SetupStyle, Color.CYAN);
-        StyleConstants.setBold(SetupStyle, true);
-        //StyleConstants.setFontFamily(SetupStyle, "Times New Roman");
-        
-        Style action = doc.addStyle("action", SetupStyle);
-        StyleConstants.setBackground(action, Color.ORANGE);
-        
-        Style event = doc.addStyle("event", SetupStyle);
-        StyleConstants.setBackground(event, Color.MAGENTA);
-        
-        Style time = doc.addStyle("time", SetupStyle);
-        StyleConstants.setBackground(time, Color.YELLOW);
+		DefaultStyle = def;
 
-        /* Style s = doc.addStyle("italic", regular);
-        StyleConstants.setItalic(s, true); */
+		SetupStyle = doc.addStyle("setup", def);
+		StyleConstants.setBackground(SetupStyle, Color.CYAN);
+		StyleConstants.setBold(SetupStyle, true);
+		// StyleConstants.setFontFamily(SetupStyle, "Times New Roman");
 
-    }
+		Style action = doc.addStyle("action", SetupStyle);
+		StyleConstants.setBackground(action, Color.ORANGE);
+
+		Style event = doc.addStyle("event", SetupStyle);
+		StyleConstants.setBackground(event, Color.MAGENTA);
+
+		Style time = doc.addStyle("time", SetupStyle);
+		StyleConstants.setBackground(time, Color.YELLOW);
+
+		/*
+		 * Style s = doc.addStyle("italic", regular);
+		 * StyleConstants.setItalic(s, true);
+		 */
+
+	}
+
+	/**
+	 * @param text
+	 * @throws Exception
+	 */
+	private void processText(String text) throws Exception {
+		clearStyles();
+		/*
+		 * Recognise modal sentences
+		 */
+		StaticDynamicClassifier myclassifier = StaticDynamicClassifier
+				.getInstance();
+		String lines = text;
+		/*
+		 * String[] modalVerbs = {"can", "could", "may", "might", "must",
+		 * "shall", "should", "will", "would", "have to", "has to", "had to",
+		 * "need"}; for (String string : modalVerbs) { if
+		 * (lines.contains(string)) { System.out.println("Found bad word: " +
+		 * string + "."); } }
+		 */
+		/* tag with pos tags */
+
+		StanfordCoreNLP pipeline = null;
+		try {
+			pipeline = myclassifier.getPipeline();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Annotation mydoc = new Annotation(lines);
+		pipeline.annotate(mydoc);
+		java.util.List<CoreMap> sentences = mydoc
+				.get(SentencesAnnotation.class);
+
+		for (CoreMap sentence : sentences) {
+			// traversing the words in the current sentence
+			// a CoreLabel is a CoreMap with additional token-specific labels
+			for (CoreLabel item : sentence.get(TokensAnnotation.class)) {
+				// this is the text of the token
+				// String word = item.get(TextAnnotation.class);
+				// this is the POS tag of the token
+				String pos = item.get(PartOfSpeechAnnotation.class);
+				if (pos.equals("VBG")) {
+					System.out.println(item.getString(TextAnnotation.class)
+							+ " is a gerund.");
+					markText(item.beginPosition(), item.endPosition());
+				}
+				// this is the NER label of the token
+				// String ne = item.get(NamedEntityTagAnnotation.class);
+
+				// System.out.println(word);
+			}
+		}
+
+	}
+
+	private void clearStyles() {
+		StyledDocument doc = (StyledDocument) txtEditor.getDocument();
+		doc.setCharacterAttributes(0, doc.getLength(), DefaultStyle, true);
+	}
+
+	private void markText(int beginPosition, int endPosition) {
+		int length = endPosition - beginPosition;
+		StyledDocument doc = (StyledDocument) txtEditor.getDocument();
+		try {
+			// System.out.println(txtEditor.getText(beginPosition, length));
+			System.err.println(doc.getText(beginPosition, length));
+		} catch (BadLocationException e) {
+			System.err.println("Bad location: " + beginPosition + " "
+					+ endPosition);
+		}
+
+		doc.setCharacterAttributes(beginPosition, length,
+				doc.getStyle("action"), true);
+		nop();
+	}
+
+	private void nop() {
+	}
+
+	private void stopAndPrintStopWatch() {
+		// TODO Auto-generated method stub
+		org.joda.time.DateTime now = org.joda.time.DateTime.now();
+		long diff = now.getMillis() - stopwatch.getMillis();
+		System.err.println("Stopwatch: " + (diff) + " ms.");
+	}
+
+	private void startStopWatch() {
+		stopwatch = org.joda.time.DateTime.now();
+	}
 
 }
