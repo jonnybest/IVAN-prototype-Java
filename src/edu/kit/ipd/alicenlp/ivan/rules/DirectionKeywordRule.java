2d2107bc2dab1cccbc0dae6770b439e9ac4e5006
jonnybest <jonathan.best@student.kit.edu>
2013-09-30 14:02:14 +0200

    getter and setter for keyword rule

--- src/edu/kit/ipd/alicenlp/ivan/rules/DirectionKeywordRule.java
+++ src/edu/kit/ipd/alicenlp/ivan/rules/DirectionKeywordRule.java
@@ -44,36 +44,45 @@ public class DirectionKeywordRule extends BaseRule implements IGraphRule {
 			// if the root matches our keywords, use it.
 			if (root.lemma().equalsIgnoreCase(kw)) {
 				// save the found verb. I assume that these verbs are always roots
-				verb = root;
-				
-				// TODO: implement extraction
-				subject = getSubject(graph);
+				setVerb(root);
+
+				setSubject(getSubject(graph));
 				// 1. check for and extract "towards"
-				List<IndexedWord> towlistsubj = getPrepRelations(subject, graph, "towards");
+				List<IndexedWord> towlistsubj = getPrepRelations(getSubject(), graph, "towards");
 				List<IndexedWord> towlistroot = getPrepRelations(root, graph, "towards");
 				if (towlistsubj.size() > 0) {
-					direction = getNounPhrase(towlistsubj.get(0), Sentence);
+					setDirection(getNounPhrase(towlistsubj.get(0), Sentence));
+					return true;
 				} 
 				else if(towlistroot.size() > 0)
 				{
-					direction = getNounPhrase(towlistroot.get(0), Sentence);
+					setDirection(getNounPhrase(towlistroot.get(0), Sentence));
+					return true;
 				}
 				// 2. check for and extract adverbial modifier
 				IndexedWord advmod = getAdvMod(root, graph);
 				if (advmod != null) {
-					direction = printSubGraph(advmod, graph);
+					setDirection(printSubGraph(advmod, graph));
+					return true;
 				}
 				// 3. check for and extract "to"
-				List<IndexedWord> tolistsubj = getPrepRelations(subject, graph, "to");
+				List<IndexedWord> tolistsubj = getPrepRelations(getSubject(), graph, "to");
 				List<IndexedWord> tolistroot = getPrepRelations(root, graph, "to");
 				if (tolistsubj.size() > 0) {
-					direction = getNounPhrase(tolistsubj.get(0), Sentence);
+					setDirection(getNounPhrase(tolistsubj.get(0), Sentence));
+					return true;
 				} 
 				else if(tolistroot.size() > 0)
 				{
-					direction = getNounPhrase(tolistroot.get(0), Sentence);
+					setDirection(getNounPhrase(tolistroot.get(0), Sentence));
+					return true;
 				}
 				// 4. check for and extract direct object
+				IndexedWord dobj = getDirectObject(root, graph);
+				if (dobj != null) {
+					setDirection(printSubGraph(dobj, graph));
+					return true;
+				}
 				// stop after the first match
 				break;
 			}
@@ -81,4 +90,45 @@ public class DirectionKeywordRule extends BaseRule implements IGraphRule {
 		return false;
 	}
 
+	/**
+	 * @return the direction
+	 */
+	public String getDirection() {
+		return direction;
+	}
+
+	/**
+	 * @param direction the direction to set
+	 */
+	private void setDirection(String direction) {
+		this.direction = direction;
+	}
+
+	/**
+	 * @return the verb
+	 */
+	public IndexedWord getVerb() {
+		return verb;
+	}
+
+	/**
+	 * @param verb the verb to set
+	 */
+	private void setVerb(IndexedWord verb) {
+		this.verb = verb;
+	}
+
+	/**
+	 * @return the subject
+	 */
+	public IndexedWord getSubject() {
+		return subject;
+	}
+
+	/**
+	 * @param subject the subject to set
+	 */
+	private void setSubject(IndexedWord subject) {
+		this.subject = subject;
+	}
 }
