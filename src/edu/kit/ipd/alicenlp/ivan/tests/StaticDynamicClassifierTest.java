f0d06da0a7e3ee965a3be98242a352606b0c51d9
jonnybest <jonathan.best@student.kit.edu>
2013-10-08 21:04:04 +0200

    stub für weitere testklasse

---
+++ src/edu/kit/ipd/alicenlp/ivan/tests/StaticDynamicClassifierTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package edu.kit.ipd.alicenlp.ivan.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Jonny
+ *
+ */
+public class StaticDynamicClassifierTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link edu.kit.ipd.alicenlp.ivan.analyzers.StaticDynamicClassifier#classifySentence(edu.stanford.nlp.ling.IndexedWord, edu.stanford.nlp.util.CoreMap)}.
+	 */
+	@Test
+	public void testClassifySentence() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
