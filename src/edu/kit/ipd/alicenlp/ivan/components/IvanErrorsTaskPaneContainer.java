8205455c8e7a4aee8589a80be8c7fe6cbb63cfd5
jonnybest <jonathan.best@student.kit.edu>
2013-10-10 15:12:39 +0200

    wishlist comment Incomplete - email : build errors feature [Jonny B <mail@jonathanbest.de>]

--- src/edu/kit/ipd/alicenlp/ivan/components/IvanErrorsTaskPaneContainer.java
+++ src/edu/kit/ipd/alicenlp/ivan/components/IvanErrorsTaskPaneContainer.java
@@ -5,7 +5,11 @@ package edu.kit.ipd.alicenlp.ivan.components;
 
 import org.jdesktop.swingx.JXTaskPaneContainer;
 
-/**
+/** This is a special JXTaskPaneContainer, which can display errors and warnings that occur in IVAN.
+ * It provides an cues to the UI where to render errors (line numbers or character offsets),
+ * actions for fixing and ignoring errors, and a serializeable state for easy instrumentation.
+ * 
+ * JXTaskPaneContainer provides an elegant view to display a list of tasks ordered by groups (org.jdesktop.swingx.JXTaskPanes). 
  * @author Jonny
  *
  */
