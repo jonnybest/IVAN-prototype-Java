ac641de56fcc1ec5f617492122d045bb8367581b
jonnybest <jonathan.best@student.kit.edu>
2013-10-11 11:58:09 +0200

    displays labels and actions. I had to fuck with some dependencies, but now it displays labels and actions

--- src/edu/kit/ipd/alicenlp/ivan/components/IvanErrorsTaskPaneContainer.java
+++ src/edu/kit/ipd/alicenlp/ivan/components/IvanErrorsTaskPaneContainer.java
@@ -3,16 +3,34 @@
  */
 package edu.kit.ipd.alicenlp.ivan.components;
 
-import java.awt.Component;
+import java.awt.BorderLayout; 
+import java.awt.Dimension; 
+import java.awt.Font; 
 import java.awt.Label;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
 
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.swing.JEditorPane; 
+import javax.swing.JFrame; 
+import javax.swing.JPanel; 
+import javax.swing.JScrollPane; 
+import javax.swing.SwingUtilities; 
+import javax.swing.UIManager; 
+import javax.swing.text.html.HTMLDocument; 
+ 
 
-import org.jdesktop.swingx.JXTaskPane;
-import org.jdesktop.swingx.JXTaskPaneContainer;
+
+
+import org.jdesktop.application.Action;
+import org.jdesktop.application.Application; 
+import org.jdesktop.application.ApplicationActionMap; 
+import org.jdesktop.swingx.JXLabel;
+import org.jdesktop.swingx.JXTaskPane; 
+import org.jdesktop.swingx.JXTaskPaneContainer; 
 
 /** This is a special JXTaskPaneContainer, which can display errors and warnings that occur in IVAN.
  * It provides an cues to the UI where to render errors (line numbers or character offsets),
@@ -25,12 +43,42 @@ import org.jdesktop.swingx.JXTaskPaneContainer;
 @SuppressWarnings("serial")
 public class IvanErrorsTaskPaneContainer extends JXTaskPaneContainer {
 
-	/**
-	 * @author Jonny
-	 *
-	 */
 	public class IvanErrorInstance {
+
+		final public String Category;
+		final public List<CodePoint> Codepoints;
+
+		public IvanErrorInstance(String category, List<CodePoint> codepoints)  
+		{
+			Category = category;
+			Codepoints = codepoints;
+		}
 		
+		@Override
+		public boolean equals(Object obj) {
+			// TODO Auto-generated method stub
+			return super.equals(obj);
+		}
+	}
+
+	private Map<String, JXTaskPane> mypanes = new TreeMap<String, JXTaskPane>();
+	private Set<IvanErrorInstance> bagofProblems = new HashSet<IvanErrorInstance>();
+	final private Font errorInfoFont = new Font("Calibri", 0, 11);
+
+	public class CodePoint extends Tuple<Integer, Integer>{
+
+		public CodePoint(Integer x, Integer y) {
+			super(x, y);		}
+		
+	}
+	
+	public class Tuple<X, Y> { 
+		public final X x; 
+		public final Y y; 
+		public Tuple(X x, Y y) { 
+			this.x = x; 
+			this.y = y; 
+		} 
 	}
 
 	/** This class represents a catergory if errors.
@@ -54,18 +102,6 @@ public class IvanErrorsTaskPaneContainer extends JXTaskPaneContainer {
 	 */
 	public IvanErrorsTaskPaneContainer() {
 	}
-
-	@Override
-	public void add(JXTaskPane group) {
-		// TODO Auto-generated method stub
-		super.add(group);
-	}
-	
-	@Override
-	public void remove(JXTaskPane group) {
-		// TODO Auto-generated method stub
-		super.remove(group);
-	}
 	
 	/** This method allows printing all the currently displayed errors and warnings in a readable, diff-able manner 
 	 * 
@@ -85,16 +121,69 @@ public class IvanErrorsTaskPaneContainer extends JXTaskPaneContainer {
 		return super.toString();
 	}
 
+	/** Create a category of errors which should be presented to the user in a single taskpane. 
+	 * This method should only be called once per runtime and per category. 
+	 * 
+	 * @param title
+	 * @param description
+	 */
 	public void createCategory(String title, String description) {
 		JXTaskPane pane = new JXTaskPane();
 		pane.setTitle(title);
-		pane.add(new Label(description));
-		Map<String, JXTaskPane> mypanes = null;
+		JXLabel lbl = new JXLabel(description);
+		lbl.setLineWrap(true);
+		
+		lbl.setFont(errorInfoFont);
+		pane.add(lbl);		
 		mypanes.put(title, pane);
 	}
 
-	public void createProblem(String string, String string2, int i, int j) {
-		// TODO Auto-generated method stub
+	public boolean createProblem(String category, String errormsg, List<CodePoint> codepoints) {
+		JXTaskPane tsk = mypanes.get(category);
+		if(tsk != null)
+		{
+			IvanErrorInstance error = new IvanErrorInstance(category, codepoints);
+			boolean present = this.bagofProblems.contains(error);
+			if(!present){
+				bagofProblems.add(error);
+				createQuickfixes(error);
+				return true;
+			}
+			return false;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	private void createQuickfixes(IvanErrorInstance error) {
+		IvanErrorsTaskPaneContainer tpc = this;
+		JXTaskPane tsk = mypanes.get(error.Category);
 		
+		// createTaskPaneDemo()
+		// "System" GROUP 
+        JXTaskPane systemGroup = tsk; 
+        systemGroup.setName("systemGroup"); 
+        tpc.add(systemGroup); 
+        
+        // bind()
+        ApplicationActionMap map = Application.getInstance().getContext().getActionMap(this); 
+        
+        systemGroup.add(map.get("email")); 
+        systemGroup.add(map.get("delete")); 
+	}
+	
+    @Action 
+    public void email() { } 
+     
+    @Action 
+    public void delete() { } 
+
+	public boolean createProblem(String category, String errormsg, CodePoint codepoint) {
+		return createProblem(category, errormsg, Arrays.asList(new CodePoint[]{codepoint}));
+	}
+
+	public boolean createProblem(String category, String errormsg, int x, int y) {
+		return createProblem(category, errormsg, new CodePoint(x, y));
 	}
 }
