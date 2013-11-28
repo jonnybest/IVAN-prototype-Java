0097a022240242b9ed54be3e81955b59e90c2dba
jonnybest <jonathan.best@student.kit.edu>
2013-11-28 15:52:10 +0100

    comment

--- src/edu/kit/ipd/alicenlp/ivan/rules/IErrorRule.java
+++ src/edu/kit/ipd/alicenlp/ivan/rules/IErrorRule.java
@@ -2,8 +2,17 @@ package edu.kit.ipd.alicenlp.ivan.rules;
 
 import edu.kit.ipd.alicenlp.ivan.data.ErrorMessageAnnotation;
 
+/** This is the interface for rules that indicate errors 
+ * 
+ * @author Jonny
+ *
+ */
 public interface IErrorRule {
 
+	/** Gets a message
+	 * 
+	 * @return A error message
+	 */
 	ErrorMessageAnnotation getErrorMessage();
 	
 }
