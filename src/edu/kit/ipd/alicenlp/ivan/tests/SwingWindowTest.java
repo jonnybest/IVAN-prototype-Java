be1cc33c8f2a0c6c42cc4bc16abf6f21caa45e81
jonnybest <jonathan.best@student.kit.edu>
2013-11-28 13:51:55 +0100

    comments and refactored SwingWindowTest.java

--- src/edu/kit/ipd/alicenlp/ivan/tests/SwingWindowTest.java
+++ src/edu/kit/ipd/alicenlp/ivan/tests/SwingWindowTest.java
@@ -4,26 +4,22 @@
 package edu.kit.ipd.alicenlp.ivan.tests;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import edu.kit.ipd.alicenlp.ivan.SwingWindow;
 
-/**
+/** Tests the Main window
+ * 
  * @author Jonny
  *
  */
 public class SwingWindowTest {
 
-	/**
-	 * @throws java.lang.Exception
+	/** Tests the file converter for creating git refs
+	 * 
 	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
 	@Test
 	public void testFile2Ref() {
 		assertThat(SwingWindow.file2ref("[a]s me..thing^?*:"), is("asome.thingoooo"));
