eddb03e67b59f708f762c7b00072d39c99961652
jonnybest <jonathan.best@student.kit.edu>
2013-11-28 15:33:05 +0100

    utility extrahiert

--- src/edu/kit/ipd/alicenlp/ivan/rules/TowardsPresentRule.java
+++ src/edu/kit/ipd/alicenlp/ivan/rules/TowardsPresentRule.java
@@ -3,20 +3,22 @@
  */
 package edu.kit.ipd.alicenlp.ivan.rules;
 
+import static edu.kit.ipd.alicenlp.ivan.rules.BaseRule.getNounPhrase;
+import static edu.kit.ipd.alicenlp.ivan.rules.BaseRule.getPrepRelations;
+
 import java.util.List;
 
 import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
 import edu.stanford.nlp.trees.EnglishGrammaticalRelations;
-import edu.stanford.nlp.trees.EnglishGrammaticalRelations.NominalSubjectGRAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 
 /** The "towards" rule is used for finding out if there's a direction
  * @author Jonny
  *
  */
-public class TowardsPresentRule extends BaseRule implements ISentenceRule {
+public class TowardsPresentRule implements ISentenceRule {
 
 	/* (non-Javadoc)
 	 * @see edu.kit.ipd.alicenlp.ivan.rules.IGraphRule#apply(edu.stanford.nlp.util.CoreMap)
