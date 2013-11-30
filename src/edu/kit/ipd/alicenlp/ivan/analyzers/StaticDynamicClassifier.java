6d8f45ebd898aedd44e29843b2c7adda9b1f716e
jonnybest <jonathan.best@student.kit.edu>
2013-11-30 13:30:48 +0100

    new annotation for sentence classes

--- src/edu/kit/ipd/alicenlp/ivan/analyzers/StaticDynamicClassifier.java
+++ src/edu/kit/ipd/alicenlp/ivan/analyzers/StaticDynamicClassifier.java
@@ -19,6 +19,7 @@ import net.sf.extjwnl.data.Synset;
 import net.sf.extjwnl.dictionary.Dictionary;
 import edu.kit.ipd.alicenlp.ivan.data.IvanAnnotations.*;
 import edu.kit.ipd.alicenlp.ivan.data.InitialState;
+import edu.kit.ipd.alicenlp.ivan.data.IvanAnnotations;
 import edu.kit.ipd.alicenlp.ivan.data.IvanErrorMessage;
 import edu.kit.ipd.alicenlp.ivan.rules.BaseRule;
 import edu.kit.ipd.alicenlp.ivan.rules.EntitiesSynonymsErrorRule;
@@ -99,7 +100,6 @@ public class StaticDynamicClassifier extends IvanAnalyzer
 			System.out.println("Event found");
 			// since we only support one classification, return the classification instantly
 			return Classification.EventDescription;
-			//sentence.set(Classification.class, Classification.EventDescription);
 		}
 		
 		// does this sentence explicitly reference time or duration?
@@ -351,7 +351,10 @@ public class StaticDynamicClassifier extends IvanAnalyzer
 			// process 
 			try {
 				Classification sentenceclass = classifySentenceAnnotation(sentence);
+				// FIXME: this call is deprecated
 				sentence.set(Classification.class, sentenceclass);
+				// this is the proper way to annotate 
+				sentence.set(IvanAnnotations.SentenceClassificationAnnotation.class, sentenceclass);
 			} catch (JWNLException e) {
 				// no classification for this sentence then :(
 				log(Redwood.ERR, "Error while classifying sentences.", e);
--- src/edu/kit/ipd/alicenlp/ivan/data/IvanAnnotations.java
+++ src/edu/kit/ipd/alicenlp/ivan/data/IvanAnnotations.java
@@ -2,46 +2,64 @@ package edu.kit.ipd.alicenlp.ivan.data;
 
 import java.util.List;
 
+import edu.kit.ipd.alicenlp.ivan.analyzers.IvanAnalyzer.Classification;
 import edu.stanford.nlp.util.TypesafeMap;
 
-/** Set of common annotations for Input & Verify Alice NLP.
- * The classes defined here are typesafe keys for getting and setting annotation values. These classes need not be instantiated outside of this class.
+/**
+ * Set of common annotations for Input & Verify Alice NLP. The classes defined
+ * here are typesafe keys for getting and setting annotation values. These
+ * classes need not be instantiated outside of this class.
  * 
  * @author Jonny
  * @see edu.stanford.nlp.ling.CoreAnnotations
- *
+ * 
  */
 public class IvanAnnotations {
 
+	/**
+	 * This class may be used to retrieve document-wide errors. Find it in the
+	 * Annotation map.
+	 * 
+	 * @author Jonny
+	 * 
+	 */
+	public class DocumentErrorAnnotation implements
+			TypesafeMap.Key<List<IvanErrorMessage>> {
 
-/**
- * @author Jonny
- *
- */
-public class DocumentErrorAnnotation implements TypesafeMap.Key<List<IvanErrorMessage>>
-{
+	}
 
-}
-/**
- * @author Jonny
- *
- */
-public class IvanEntitiesAnnotation implements TypesafeMap.Key<InitialState>
-{
-	
-}
+	/**
+	 * This annotation contains the entities found in this document. Find it in
+	 * the annotation map.
+	 * 
+	 * @author Jonny
+	 * 
+	 */
+	public class IvanEntitiesAnnotation implements
+			TypesafeMap.Key<InitialState> {
 
+	}
 
-/**
- * @author Jonny
- *
- */
-public class ErrorMessageAnnotation implements TypesafeMap.Key<IvanErrorMessage>
-{
-	
+	/**
+	 * This annotation indicates an issue with a sentence or a word. Find it in
+	 * the sentence annotation.
+	 * 
+	 * @author Jonny
+	 * 
+	 */
+	public class ErrorMessageAnnotation implements
+			TypesafeMap.Key<IvanErrorMessage> {
 
-}
+	}
 
+	/**
+	 *  This annotation indicates what this sentence is about. 
+	 *  
+	 * @author Jonny
+	 *
+	 */
+	public class SentenceClassificationAnnotation implements
+			TypesafeMap.Key<Classification> {
 
-
+	}
 }
