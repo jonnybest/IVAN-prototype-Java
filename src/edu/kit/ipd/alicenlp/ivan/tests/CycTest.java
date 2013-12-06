fc448b2eddc5a8db8b850ac510bfed25b78c5902
jonnybest <jonathan.best@student.kit.edu>
2013-12-06 19:48:33 +0100

    spekulative imports für cyc test

--- src/edu/kit/ipd/alicenlp/ivan/tests/CycTest.java
+++ src/edu/kit/ipd/alicenlp/ivan/tests/CycTest.java
@@ -2,6 +2,14 @@ package edu.kit.ipd.alicenlp.ivan.tests;
 
 import static org.junit.Assert.*;
 
+ import java.util.*;
+ import java.net.*;
+ import java.io.*;
+ import java.text.*;
+ import org.opencyc.cycobject.*;
+ 
+ import org.opencyc.util.*;
+
 import org.junit.Test;
 
 public class CycTest {
