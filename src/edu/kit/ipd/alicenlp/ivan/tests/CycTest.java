94c6b0afe4e1b27be6229c11c0ec9dcc06d21e6c
jonnybest <jonathan.best@student.kit.edu>
2013-12-06 13:51:47 +0100

    tests für ssh und cyc (stubs)

---
+++ src/edu/kit/ipd/alicenlp/ivan/tests/CycTest.java
@@ -0,0 +1,14 @@
+package edu.kit.ipd.alicenlp.ivan.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CycTest {
+
+	@Test
+	public final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ src/edu/kit/ipd/alicenlp/ivan/tests/SshTest.java
@@ -0,0 +1,22 @@
+package edu.kit.ipd.alicenlp.ivan.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/** This class tests SSH connections.
+ * 
+ * @author Jonny
+ *
+ */
+public class SshTest {
+
+	@Test
+	public final void test() {
+		
+		
+		
+		fail("Not yet implemented"); // TODO
+	}
+
+}
