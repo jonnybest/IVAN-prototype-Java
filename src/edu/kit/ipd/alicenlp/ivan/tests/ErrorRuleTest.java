b452648d8df821cde2afaaa1685e790f600d0d96
jonnybest <jonathan.best@student.kit.edu>
2014-01-12 06:03:00 +0100

    introducing a new unit test for errors

---
+++ src/edu/kit/ipd/alicenlp/ivan/tests/ErrorRuleTest.java
@@ -0,0 +1,15 @@
+package edu.kit.ipd.alicenlp.ivan.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ErrorRuleTest {
+
+	@Test
+	public final void testApply() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	
+}
