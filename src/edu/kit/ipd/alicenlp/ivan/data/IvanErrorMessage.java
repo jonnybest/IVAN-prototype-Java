3411b0352d5c56c468af326e571c48815cc34f9b
jonnybest <jonathan.best@student.kit.edu>
2014-01-12 05:37:55 +0100

    REFactor: better constructors for IvanErrorMessage

--- src/edu/kit/ipd/alicenlp/ivan/data/IvanErrorMessage.java
+++ src/edu/kit/ipd/alicenlp/ivan/data/IvanErrorMessage.java
@@ -2,6 +2,10 @@ package edu.kit.ipd.alicenlp.ivan.data;
 
 import edu.stanford.nlp.ie.machinereading.structure.Span;
 
+/** This class is about telling the user where things went wrong.
+ * @author Jonny
+ *
+ */
 public class IvanErrorMessage 
 {
 	private final String Message;
@@ -9,6 +13,10 @@ public class IvanErrorMessage
 	
 	private IvanErrorType type = IvanErrorType.UNKNOWN;
 
+	/** A human-readable error message for this specific issue.
+	 * 
+	 * @return Help (hopefully)
+	 */
 	public String getMessage() {
 		return Message;
 	}
@@ -21,21 +29,49 @@ public class IvanErrorMessage
 		return Range;
 	}
 	
-	public IvanErrorMessage(Integer start, Integer end, String message)
+	/** Create a new error message.
+	 * 
+	 * @param ErrorType A category which describes the kind of error.
+	 * @param start Where the offending words begin
+	 * @param end Where the offending words end
+	 * @param ErrorMessage What this error is all about. Human-readable, helpful text, please.
+	 */
+	public IvanErrorMessage(Integer start, Integer end, String ErrorMessage)
 	{
-		Range = Span.fromValues(start, end);
-		Message = message;
+		this(IvanErrorType.UNKNOWN, start, end, ErrorMessage);
 	}
 
-	public IvanErrorMessage(Span errorspan, String message) {
-		Range = errorspan;
-		Message = message;
+	/** Create a new error message. The type will be "IvanErrorType.UNKNOWN"
+	 * 
+	 * @param errorspan Where the offending words begin and end
+	 * @param ErrorMessage What this error is all about. Human-readable, helpful text, please.
+	 */
+	public IvanErrorMessage(Span errorspan, String ErrorMessage) {
+		this(IvanErrorType.UNKNOWN, errorspan, ErrorMessage);
 	}
-	
-	public IvanErrorMessage(IvanErrorType errorId, Span errorspan, String message) {
-		type = errorId;
+
+	/** Create a new error message.
+	 * 
+	 * @param ErrorType A category which describes the kind of error.
+	 * @param errorspan Where the offending words begin and end
+	 * @param ErrorMessage What this error is all about. Human-readable, helpful text, please.
+	 */
+	public IvanErrorMessage(IvanErrorType ErrorType, Span errorspan, String ErrorMessage) {
+		type = ErrorType;
 		Range = errorspan;
-		Message = message;
+		Message = ErrorMessage;
+	}
+
+	/** Create a new error message.
+	 * 
+	 * @param ErrorType A category which describes the kind of error.
+	 * @param Begin Where the offending words begin
+	 * @param End Where the offending words end
+	 * @param ErrorMessage What this error is all about. Human-readable, helpful text, please.
+	 */
+	public IvanErrorMessage(IvanErrorType ErrorType, Integer Begin,
+			Integer End, String ErrorMessage) {
+		this(ErrorType, Span.fromValues(Begin, End), ErrorMessage);
 	}
 
 	@Override
@@ -43,6 +79,9 @@ public class IvanErrorMessage
 		return Range.toString() +" "+ getMessage();
 	}
 
+	/** A category which describes the kind of error.
+	 * @return An instance of the Enum IvanErrorType
+	 */
 	public IvanErrorType getType() {
 		return type;
 	}
