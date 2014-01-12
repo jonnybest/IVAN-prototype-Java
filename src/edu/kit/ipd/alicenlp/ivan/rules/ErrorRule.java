613be29b17fa79c8a3ae2ccfe632054d8ee318a3
jonnybest <jonathan.best@student.kit.edu>
2014-01-12 06:49:29 +0100

    NEW fragment rule done. +Test

--- src/edu/kit/ipd/alicenlp/ivan/rules/ErrorRule.java
+++ src/edu/kit/ipd/alicenlp/ivan/rules/ErrorRule.java
@@ -3,64 +3,100 @@
  */
 package edu.kit.ipd.alicenlp.ivan.rules;
 
-import static edu.stanford.nlp.trees.EnglishGrammaticalRelations.*;
+import static edu.stanford.nlp.trees.EnglishGrammaticalRelations.CONJUNCT;
+import static edu.stanford.nlp.trees.EnglishGrammaticalRelations.COPULA;
+import static edu.stanford.nlp.trees.EnglishGrammaticalRelations.PREDICATE;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-import edu.kit.ipd.alicenlp.ivan.data.IvanAnnotations.ErrorMessageAnnotation;
 import edu.kit.ipd.alicenlp.ivan.data.IvanErrorMessage;
 import edu.kit.ipd.alicenlp.ivan.data.IvanErrorType;
 import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.ling.CoreAnnotations.DocIDAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.CategoryAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.ling.IndexedWord;
-import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.BasicDependenciesAnnotation;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
-import edu.stanford.nlp.trees.EnglishGrammaticalRelations;
 import edu.stanford.nlp.trees.EnglishGrammaticalRelations.NominalSubjectGRAnnotation;
 import edu.stanford.nlp.trees.GrammaticalRelation;
+import edu.stanford.nlp.trees.LabeledScoredTreeNode;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 
 /**
  * @author Jonny
- *
+ * 
  */
-public class ErrorRule implements ISentenceRule, IErrorRule
-{
+public class ErrorRule implements ISentenceRule, IErrorRule {
 
 	IvanErrorMessage msg;
-	
-	/* (non-Javadoc)
-	 * @see edu.kit.ipd.alicenlp.ivan.rules.ISentenceRule#apply(edu.stanford.nlp.util.CoreMap)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.kit.ipd.alicenlp.ivan.rules.ISentenceRule#apply(edu.stanford.nlp.
+	 * util.CoreMap)
 	 */
 	@Override
 	public boolean apply(CoreMap sentence) {
 		// check for a graph
-		if(applyNeedsGraph(sentence))
-			return true;		
+		if (applyNeedsGraph(sentence))
+			return true;
 		// checking roots:
-		if(applyRoots(sentence))
+		if (applyRoots(sentence))
 			return true;
-		
-		if(applyNeedsOneVerb(sentence))
+
+		if (applyNeedsOneVerb(sentence))
 			return true;
-		
+
 		// TODO: sentences with the "fragment" tree annotation are not desirable
 		// also todo: find out what that fragment thing looks like
-		
-		// verbs that are joined by a conjunction are probably missing arguments and cannot be properly parsed with dependencies
-		if(applyCCVerbs(sentence))
+
+		// verbs that are joined by a conjunction are probably missing arguments
+		// and cannot be properly parsed with dependencies
+		if (applyCCVerbs(sentence))
 			return true;
-		
-		// first person sentences do not have a verb that reflects the action on the scene
-		if(applyNo1stPerson(sentence))
+
+		// first person sentences do not have a verb that reflects the action on
+		// the scene
+		if (applyNo1stPerson(sentence))
 			return true;
-		
+
 		// everything seems to be fine.
+		return false;
+	}
+
+	/**
+	 * Marks sentences with undesirable Penn tags. Specifically: FRAG - Fragment
+	 * (because too much information is missing)
+	 * 
+	 * @param sentence
+	 *            input
+	 * @return FALSE if all the tags are "allowed"
+	 */
+	public boolean applyBadPennTags(CoreMap sentence) {
+		// maybe do these too?
+		// UCP - Unlike Coordinated Phrase (because it's a pairing of unlike constituents)
+		// SBAR - Subordinate clause, complementizer or WH- and sentence (because they do not fit the genre)
+
+		// fetch a tree
+		Tree t = sentence.get(TreeAnnotation.class);
+		// iterate through all the nodes
+		for (LabeledScoredTreeNode thing : t.toArray(new LabeledScoredTreeNode[]{})) {
+			// fetches the label which contains tags
+			CoreLabel lbl = (CoreLabel) thing.label();
+			// fetches the tag which is called a "category" here
+			String cat = lbl.get(CategoryAnnotation.class);
+			// if the category is "Fragment", we don't want it
+			if(cat.equals("FRAG"))
+				return true;
+		}
+		// meh. didn't find anything
 		return false;
 	}
 
@@ -70,10 +106,12 @@ public class ErrorRule implements ISentenceRule, IErrorRule
 	 */
 	public boolean applyNo1stPerson(CoreMap sentence) {
 		// no 1st person descriptions (because of missing verb)
-		if(ErrorRule.is1stPerson(sentence.get(CollapsedCCProcessedDependenciesAnnotation.class)))
-		{
-			error(IvanErrorType.STYLE, "Sentences with \"I\" are difficult to understand. "
-					+ "Please try not to use \"I\" in a sentence.", sentence);
+		if (ErrorRule.is1stPerson(sentence
+				.get(CollapsedCCProcessedDependenciesAnnotation.class))) {
+			error(IvanErrorType.STYLE,
+					"Sentences with \"I\" are difficult to understand. "
+							+ "Please try not to use \"I\" in a sentence.",
+					sentence);
 			return true;
 		}
 		return false;
@@ -85,11 +123,12 @@ public class ErrorRule implements ISentenceRule, IErrorRule
 	 */
 	public boolean applyCCVerbs(CoreMap sentence) {
 		// conjoined verbs are not proper use and lead to parameter errors
-		if(hasCC(getMainVerb(sentence), sentence))
-		{
-			error(IvanErrorType.GRAPH, "The verbs in this sentence both refer to the same thing."
-					+ " Instead, please use one verb per sentence."
-					+ " Otherwise we'll probably get it wrong.", sentence);
+		if (hasCC(getMainVerb(sentence), sentence)) {
+			error(IvanErrorType.GRAPH,
+					"The verbs in this sentence both refer to the same thing."
+							+ " Instead, please use one verb per sentence."
+							+ " Otherwise we'll probably get it wrong.",
+					sentence);
 			return true;
 		}
 		return false;
@@ -101,23 +140,8 @@ public class ErrorRule implements ISentenceRule, IErrorRule
 	 */
 	public boolean applyNeedsOneVerb(CoreMap sentence) {
 		// checking verbs. each sentence needs at least one
-		if((getMainVerb(sentence) == null))
-		{
+		if ((getMainVerb(sentence) == null)) {
 			error(IvanErrorType.GRAPH, "This sentence needs a verb.", sentence);
-			return true;
-		}
-		return false;
-	}
-	
-	/**
-	 * @param sentence
-	 * @return
-	 */
-	public boolean applyNeedsGraph(CoreMap sentence) {
-		// checking the graph. if the sentence does not have a graph, we can't process it
-		if(sentence.get(CollapsedCCProcessedDependenciesAnnotation.class).isEmpty())
-		{
-			error(IvanErrorType.GRAPH, "This sentence is intelligible. It probably needs to be longer.", sentence);
 			return true;
 		}
 		return false;
@@ -125,43 +149,64 @@ public class ErrorRule implements ISentenceRule, IErrorRule
 
 	/**
 	 * @param sentence
-	 * @return 
+	 * @return
+	 */
+	public boolean applyNeedsGraph(CoreMap sentence) {
+		// checking the graph. if the sentence does not have a graph, we can't
+		// process it
+		if (sentence.get(CollapsedCCProcessedDependenciesAnnotation.class)
+				.isEmpty()) {
+			error(IvanErrorType.GRAPH,
+					"This sentence is intelligible. It probably needs to be longer.",
+					sentence);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * @param sentence
+	 * @return
 	 */
 	public boolean applyRoots(CoreMap sentence) {
-		Collection<IndexedWord> roots = sentence.get(BasicDependenciesAnnotation.class).getRoots();
+		Collection<IndexedWord> roots = sentence.get(
+				BasicDependenciesAnnotation.class).getRoots();
 		// if the sentence has no root, it's an error
-		if(roots.size() == 0)
-		{
-			error(IvanErrorType.GRAPH, "This is not a proper sentence.", sentence);
+		if (roots.size() == 0) {
+			error(IvanErrorType.GRAPH, "This is not a proper sentence.",
+					sentence);
 			return true;
 		}
 		// if the sentence has more than one roots, we're probably missing out
-		else if(roots.size() > 1)
-		{
-			error(IvanErrorType.STYLE, "IVAN cannot handle sentences with more than a single topic."
-					+ " Try splitting the sentence into two sentences.", sentence);
+		else if (roots.size() > 1) {
+			error(IvanErrorType.STYLE,
+					"IVAN cannot handle sentences with more than a single topic."
+							+ " Try splitting the sentence into two sentences.",
+					sentence);
 			return true;
 		}
 		return false;
 	}
 
 	private static boolean hasCC(IndexedWord mainVerb, CoreMap sentence) {
-		SemanticGraph graph = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
-		return mainVerb == null ? false :graph.hasChildWithReln(mainVerb, CONJUNCT);
+		SemanticGraph graph = sentence
+				.get(CollapsedCCProcessedDependenciesAnnotation.class);
+		return mainVerb == null ? false : graph.hasChildWithReln(mainVerb,
+				CONJUNCT);
 	}
 
 	private static IndexedWord getMainVerb(CoreMap sentence) {
-		SemanticGraph graph = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
+		SemanticGraph graph = sentence
+				.get(CollapsedCCProcessedDependenciesAnnotation.class);
 		IndexedWord root = graph.getFirstRoot();
-		if(BaseRule.isPOSFamily(root, "VB"))
-		{
+		if (BaseRule.isPOSFamily(root, "VB")) {
 			return root;
 		}
 		// root is not a verb. look for a cop near root^
-		Collection<GrammaticalRelation> rels = Arrays.asList(new GrammaticalRelation[]{
-				COPULA,
-				PREDICATE});
-		List<IndexedWord> verbcandidates = graph.getChildrenWithRelns(root, rels);
+		Collection<GrammaticalRelation> rels = Arrays
+				.asList(new GrammaticalRelation[] { COPULA, PREDICATE });
+		List<IndexedWord> verbcandidates = graph.getChildrenWithRelns(root,
+				rels);
 		for (IndexedWord w : verbcandidates) {
 			return w;
 		}
@@ -169,16 +214,16 @@ public class ErrorRule implements ISentenceRule, IErrorRule
 	}
 
 	private void error(String message, CoreMap sentence) {
-		msg = new IvanErrorMessage(				
-				sentence.get(CoreAnnotations.CharacterOffsetBeginAnnotation.class), 
+		msg = new IvanErrorMessage(
+				sentence.get(CoreAnnotations.CharacterOffsetBeginAnnotation.class),
 				sentence.get(CoreAnnotations.CharacterOffsetEndAnnotation.class),
 				message);
 	}
-	
+
 	private void error(IvanErrorType type, String message, CoreMap sentence) {
 		msg = new IvanErrorMessage(
-				type,				
-				sentence.get(CoreAnnotations.CharacterOffsetBeginAnnotation.class), 
+				type,
+				sentence.get(CoreAnnotations.CharacterOffsetBeginAnnotation.class),
 				sentence.get(CoreAnnotations.CharacterOffsetEndAnnotation.class),
 				message);
 	}
@@ -187,15 +232,17 @@ public class ErrorRule implements ISentenceRule, IErrorRule
 		return msg;
 	}
 
-	/** Decides whether this sentence contains the subject "I"
+	/**
+	 * Decides whether this sentence contains the subject "I"
+	 * 
 	 * @param graph
 	 * @return
 	 */
-	public static Boolean is1stPerson(final SemanticGraph graph)
-	{
+	public static Boolean is1stPerson(final SemanticGraph graph) {
 		IndexedWord root = graph.getFirstRoot();
 		// first person = nominal subject is "I"
-		GrammaticalRelation subjclass = GrammaticalRelation.getRelation(NominalSubjectGRAnnotation.class);
+		GrammaticalRelation subjclass = GrammaticalRelation
+				.getRelation(NominalSubjectGRAnnotation.class);
 		IndexedWord subject = graph.getChildWithReln(root, subjclass);
 		return subject == null || subject.word().equalsIgnoreCase("I");
 	}
