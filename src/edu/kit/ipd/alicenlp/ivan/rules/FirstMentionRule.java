569c2f0338e685c56ce0b3eebc4464f990e58553
Jonny Best <jonathan.best@student.kit.edu>
2014-01-19 17:31:59 +0100

    new format

--- src/edu/kit/ipd/alicenlp/ivan/rules/FirstMentionRule.java
+++ src/edu/kit/ipd/alicenlp/ivan/rules/FirstMentionRule.java
@@ -68,8 +68,7 @@ public class FirstMentionRule implements IDocumentRule {
 	@Override
 	public boolean apply(Annotation doc, boolean canWrite) throws IvanException {
 
-		InitialState state = doc
-				.get(IvanAnnotations.IvanEntitiesAnnotation.class);
+		InitialState state = doc.get(IvanAnnotations.IvanEntitiesAnnotation.class);
 		if (state == null)
 			return false; // shortcut
 
@@ -77,44 +76,34 @@ public class FirstMentionRule implements IDocumentRule {
 			String handle = thing.first == null ? thing.second : thing.first;
 			ArrayList<EntityInfo> infos = state.get(handle);
 			if (infos == null)
-				throw new IvanException("State is inconsistent. Name '"
-						+ handle
-						+ "' was given but has no attached entity information.");
+				throw new IvanException("State is inconsistent. Name '" + handle + "' was given but has no attached entity information.");
 			EntityInfo earliestMention = infos.get(0);
 			EntityInfo earliestLocation = null;
 			EntityInfo earliestDirection = null;
 			for (int i = 0; i < infos.size(); i++) {
 				EntityInfo ei = infos.get(i);
 				// find first mention
-				if (ei != earliestMention
-						&& ei.getEntitySpan().isBefore(
-								earliestMention.getEntitySpan())) {
+				if (ei != earliestMention && ei.getEntitySpan().isBefore(earliestMention.getEntitySpan())) {
 					earliestMention = ei;
 				}
 				// find first location
 				if (ei.hasLocation()) {
-					if (earliestLocation == null
-							|| ei.getEntitySpan().isBefore(
-									earliestLocation.getEntitySpan())) {
+					if (earliestLocation == null || ei.getEntitySpan().isBefore(earliestLocation.getEntitySpan())) {
 						earliestLocation = ei;
 					}
 				}
 				// find first direction
 				if (ei.hasDirection()) {
-					if (earliestDirection == null
-							|| ei.getEntitySpan().isBefore(
-									earliestDirection.getEntitySpan())) {
+					if (earliestDirection == null || ei.getEntitySpan().isBefore(earliestDirection.getEntitySpan())) {
 						earliestDirection = ei;
 					}
 				}
 			}
 			// first mentions have been found
 			resultSpans.add(earliestMention.getEntitySpan().start());
-			if (earliestMention != earliestDirection
-					&& earliestDirection != null)
+			if (earliestMention != earliestDirection && earliestDirection != null)
 				resultSpans.add(earliestDirection.getEntitySpan().start());
-			if (earliestDirection != earliestLocation
-					&& earliestLocation != null)
+			if (earliestDirection != earliestLocation && earliestLocation != null)
 				resultSpans.add(earliestMention.getEntitySpan().start());
 		}
 		Collections.sort(resultSpans);
@@ -122,8 +111,7 @@ public class FirstMentionRule implements IDocumentRule {
 		List<CoreMap> sentences = doc.get(SentencesAnnotation.class);
 		for (int i = 0; i < sentences.size(); i++) {
 			CoreMap s = sentences.get(i);
-			Integer sentenceEnd = s
-					.get(CoreAnnotations.CharacterOffsetEndAnnotation.class);
+			Integer sentenceEnd = s.get(CoreAnnotations.CharacterOffsetEndAnnotation.class);
 			boolean addme = false;
 			while (mi < resultSpans.size() && resultSpans.get(mi) < sentenceEnd) {
 				addme = true;
@@ -134,8 +122,7 @@ public class FirstMentionRule implements IDocumentRule {
 				// if so, let's add the result
 				results.add(i);
 				if (canWrite) {
-					s.set(IvanAnnotations.SentenceClassificationAnnotation.class,
-							Classification.SetupDescription);
+					s.set(IvanAnnotations.SentenceClassificationAnnotation.class, Classification.SetupDescription);
 				}
 			}
 		}
