2b9c9ffffb45387f7282886635bb9f5cd1169f23
jonnybest <jonathan.best@student.kit.edu>
2014-02-12 09:08:22 +0100

    "test" test for erroreous sentences

--- src/edu/kit/ipd/alicenlp/ivan/tests/ClassificationErrorsTests.java
+++ src/edu/kit/ipd/alicenlp/ivan/tests/ClassificationErrorsTests.java
@@ -31,6 +31,7 @@ import edu.stanford.nlp.ling.CoreAnnotations.SpanAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
+import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 import edu.stanford.nlp.util.logging.PrettyLogger;
 import edu.stanford.nlp.util.logging.Redwood;
@@ -88,20 +89,103 @@ public class ClassificationErrorsTests {
 	@Test
 	public void testUnknownProblem()
 	{
-		String words = "Alice Liddell says: \"Oh...\" and then moves to the bunny.";
+		String words = "Alice answers to the bunny. "+
+				"to Alice. "+
+				"The bunny says: \"Aaaah.\" "+
+				"Alice says: \"Hey! "+
+				"A cheshire cat appears on the tree and says: \":-D\". "+
+				"The girl points at the boy. "+
+				"They wave again. "+
+				"Next she extends her right hand and shakes. "+
+				"As his last action, the boy waves. "+
+				"After raising their arms, both the boy and the girl spin quickly five times. "+
+				"When the alien is almost next to him, he says, \"...one...\". "+
+				", and takes a step towards the alien. "+
+				"The alien points at the astronaut. "+
+				"Alien off to the right. "+
+				", to which the T-Rex replies with another loud \"ROAAAAAAAAR!!\" "+
+				"The Bunny taps its foot twice. "+
+				"Sentence is in 1st person: 'I see a palm tree on the left of the screen, a mailbox in front of it. "+
+				"Right front: bunny in white colour. "+
+				"In front of bunny on right side: green broccoli. "+
+				"The bunny taps its foot. "+
+				"The Bunny taps its left foot leg twice on the ground. "+
+				"The bunny taps its left foot two times. "+
+				"The bunny taps two times on the ground. "+
+				"A very short time passes. "+
+				"The Bunny taps its foot. "+
+				"A very short time passes. "+
+				"A very short time passes. "+
+				"A very short time passes. "+
+				"The bunny taps its foot twice. "+
+				"A very short time passes. "+
+				"A very short time passes. "+
+				"The bunny taps its left foot twice. "+
+				"The cheerleader cheers. "+
+				"The penguin flaps his wings twice. "+
+				"The penguin flaps his wings once. "+
+				"The cheerleader cheers. "+
+				"After the cheerleader cheers the penguin flaps twice with his wings. "+
+				"But before the cheerleader jumps again she cheers. "+
+				"The penguin flaps its wings twice. "+
+				"After turning its head, the penguin flaps its wings once. "+
+				"When the scene starts, the cheerleader cheers. "+
+				"Before the last jump, she cheers. "+
+				"Before that and after that, he flaps his wings. "+
+				"But at the beginning of the scene, she cheers. "+
+				"Before that the penguin waves its wings two times. "+
+				"At the beginning the cheerleader cheers. "+
+				"Before the penguin flaps his wings twice, the cheerleader cheers. "+
+				"Afterwards it cheers again. "+
+				"The cheerleader cheers. "+
+				"Shortly thereafter the penguin flaps its wings twice. "+
+				"Previously it flaps its wings once. "+
+				"At first the cheerleader cheers followed by the penguin flapping wings twice. "+
+				"and moves to the middle of the foreground. "+
+				"And a penguin in the foreground to the right. "+
+				"The cheerleader cheers. "+
+				"At the beginning the cheerleader cheers slowly, turning to face the penguin. "+
+				"Both the cowboy and the camel nod four times. "+
+				"The cowboy steps in the middle of the picture. "+
+				"Then the cowboy and the camel nod for four times. "+
+				"The camel and the cowboy look at each other. "+
+				"Finally, the cowboy and the camel nod four times. "+
+				"The cowboy and the camel lower their heads four times. "+
+				"At the end the cowboy and the camel both nod 4 times. "+
+				"Afterwards both, the cowboy and the dromedary bow four times. "+
+				"Afterwards both, the cowboy and the camel nod four times. "+
+				"After the dragon says: \"What a beautiful windmill\", the dog turns to face the windmill. "+
+				"Before that the dragon turns to face the windmill. "+
+				"after she turned to face the dragon. "+
+				"Afterwards the Woman moves towards the Windmill. "+
+				"Right after that the dragon turns back to face south, flies in the air and lands back on the ground. "+
+				"The boy and the girl lower both their arms. "+
+				"The boy and the girl wave with their right arms. "+
+				"The spaceman says: “He?” "+
+				"A space ship with the label \"United States of America\" stands on the moon. "+
+				"The alien nods and says \"/%$(&)!!!\" "+
+				"while he takes a great step towards the alien. "+
+				"The astronaut says, \"...one...\" and then says, \"...giant leap for...\". "+
+				"The alien turns to face the astronaut, it says, \"/%$(&)$!!!\" "+
+				"while moving one step toward the alien. ";
+		
+		
 		System.out.println("This is what the whole document looks like:");
 		Annotation doc = TestUtilities.annotateClassifications(words);
 		for (CoreMap sentence  : doc.get(SentencesAnnotation.class)) {
 			System.out.println(sentence);
+			System.out.println(sentence.get(IvanAnnotations.ErrorMessageAnnotation.class));
+			System.out.println(sentence.get(TreeAnnotation.class));
 			System.out.println(sentence.get(CollapsedCCProcessedDependenciesAnnotation.class));
 		}
-		System.err.println("---");
-		System.out.println("And this is what the single sentence looks like:");
-		{
-			CoreMap sentence = TestUtilities.annotateSingleClassification(words);
-			System.out.println(sentence);
-			System.out.println(sentence.get(CollapsedCCProcessedDependenciesAnnotation.class));
-		}
+
+//		System.err.println("---");
+//		System.out.println("And this is what the single sentence looks like:");
+//		{
+//			CoreMap sentence = TestUtilities.annotateSingleClassification(words);
+//			System.out.println(sentence);
+//			System.out.println(sentence.get(CollapsedCCProcessedDependenciesAnnotation.class));
+//		}
 	}
 	
 	@Test
